2ce953d31d833a3ba007d4f875a3b86fc3b6ffa8
Letyriaus T Jackson <letyjack22@bossierschools.org>
2026-02-05 08:58:55 -0600

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,37 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(200, 168, 100, 80, "purple");  // bright purple for a finished platform
+    createPlatform(500, 620, 200, 20, "blue");  // baby blue for a finished platform
+    createPlatform(800, 500, 100, 50, "green");  // bold green for a finished platform
+    createPlatform(900, 370, 200, 10);
+    createPlatform(500, 500, 200, 10);
+    createPlatform(900, 610, 200, 30, "orange");  //bright orange for a finished platform
+    createPlatform(450, 300, 200, 10, "gold");  // gold for a finished platform
+    createPlatform(1200, 232, 200, 10, "brown"); //brown for a finished platform
+
 
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("steve", 1350, 50);
+    createCollectable("diamond", 200, 100, 1, 0.8);
+    createCollectable("kennedi", 80, 400,);
+    createCollectable("max", 1300, 600);
+
 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("top", 200, 500);
+    createCannon("top", 760, 440);
+    createCannon("bottom", 1142, 500);
 
 
     
